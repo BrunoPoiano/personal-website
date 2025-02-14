6b0dcbdb213b459555cc9ac7089dd94f2f7d6374
BrunoPoiano <bruno.poiano@proton.me>
2025-02-14 09:58:20 -0300

    adding light mode

--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,77 @@
-const anchor = document.getElementById("anchor");
-const eyes = document.querySelectorAll(".eye");
+document.addEventListener("DOMContentLoaded", () => {
+  ///////////////////////ligh dark theme
+  let colorScheme = localStorage.getItem("theme") || "dark";
 
-function mousemove(e) {
-  const mouxeX = e.clientX;
-  const mouxeY = e.clientY;
+  const modeButton = document.getElementById("mode-toggle");
+  const moon = document.getElementById("moon");
+  const sun = document.getElementById("sun");
 
-  const rekt = anchor.getBoundingClientRect();
-  const anchorX = rekt.left + rekt.width / 2;
-  const anchorY = rekt.top + rekt.height / 2;
+  const changeColorTheme = () => {
+    localStorage.setItem("theme", colorScheme);
+    moon.setAttribute("data-visible", colorScheme == "light");
+    sun.setAttribute("data-visible", colorScheme == "dark");
+    document.documentElement.style.setProperty("color-scheme", colorScheme);
+  };
 
-  const angleDeg = angle(mouxeX, mouxeY, anchorX, anchorY);
-  eyes.forEach((eye) => {
-    eye.style.transform = `rotate(${-90 + angleDeg}deg)`;
+  modeButton.addEventListener("click", () => {
+    if (colorScheme == "dark") {
+      colorScheme = "light";
+    } else {
+      colorScheme = "dark";
+    }
+
+    changeColorTheme();
   });
-};
 
-function angle(cx, cy, ex, ey) {
-  const dy = ey - cy;
-  const dx = ex - cx;
-  const rad = Math.atan2(dy, dx);
-  const deg = (rad * 180) / Math.PI;
+  changeColorTheme();
 
-  return deg;
-}
+  ///////////////////////Move picture eyes
+  const anchor = document.getElementById("anchor");
+  const eyes = document.querySelectorAll(".eye");
 
-function isAnchorVisible() {
-  const anchorRect = anchor.getBoundingClientRect();
-  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-  const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+  const mousemove = (e) => {
+    const mouxeX = e.clientX;
+    const mouxeY = e.clientY;
 
-  return (
-    anchorRect.top >= 0 &&
-    anchorRect.left >= 0 &&
-    anchorRect.bottom <= windowHeight &&
-    anchorRect.right <= windowWidth
-  );
-}
+    const rekt = anchor.getBoundingClientRect();
+    const anchorX = rekt.left + rekt.width / 2;
+    const anchorY = rekt.top + rekt.height / 2;
 
-function handleMouseMove(e) {
-  if (isAnchorVisible()) {
-    mousemove(e);
-  }
-}
+    const angleDeg = angle(mouxeX, mouxeY, anchorX, anchorY);
+    eyes.forEach((eye) => {
+      eye.style.transform = `rotate(${-90 + angleDeg}deg)`;
+    });
+  };
 
-addEventListener("mousemove", handleMouseMove);
+  const angle = (cx, cy, ex, ey) => {
+    const dy = ey - cy;
+    const dx = ex - cx;
+    const rad = Math.atan2(dy, dx);
+    const deg = (rad * 180) / Math.PI;
+
+    return deg;
+  };
+
+  const isAnchorVisible = () => {
+    const anchorRect = anchor.getBoundingClientRect();
+    const windowHeight =
+      window.innerHeight || document.documentElement.clientHeight;
+    const windowWidth =
+      window.innerWidth || document.documentElement.clientWidth;
+
+    return (
+      anchorRect.top >= 0 &&
+      anchorRect.left >= 0 &&
+      anchorRect.bottom <= windowHeight &&
+      anchorRect.right <= windowWidth
+    );
+  };
+
+  const handleMouseMove = (e) => {
+    if (isAnchorVisible()) {
+      mousemove(e);
+    }
+  };
+
+  addEventListener("mousemove", handleMouseMove);
+});
